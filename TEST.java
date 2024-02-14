dc82b0ed11051b5fccfbdca738714f6b69275ec3
NikolasPosavec <nikolas.posavec@gmail.com>
2024-02-14 18:26:49 -0500

    string prgm done

--- TEST.java
+++ TEST.java
@@ -1,13 +1,16 @@
-public class TEST {
-    public static void main(String[] args) {
+public class TEST 
+{
+    public static void main(String[] args) 
+    {
         String name = "Posavec, Nikolas Stjepan";
-
         int pos = name.indexOf(',');
-
+        int space2 = name.indexOf(" ", pos+2);
+        
         String lastname = name.substring(0, pos);
-        String firstname = name.substring(pos + 2, name.indexOf(' ', pos + 2));
-        String middlename = name.substring(name.indexOf(' ', pos + 2) + 1);
+        String firstname = name.substring(pos + 2, space2);
+        String middlename = name.substring(space2 + 1);
+        String minitial = middlename.substring(0,1);
 
-        System.out.println(firstname + " " + middlename + ", " + lastname);
+        System.out.println(firstname + " " + minitial + ", " + lastname);
     }
 }
