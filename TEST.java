3d42c3f7a3743fd0c3272c6daad1c1447591e9be
Nikolas Posavec <nikolas.posavec@gmail.com>
2024-02-22 16:19:04 -0500

    optimized PalindromeNP

--- TEST.java
+++ TEST.java
@@ -1,41 +1,26 @@
 import java.util.Scanner;
-
 public class TEST {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
 
         System.out.println("What is your word?");
-        String testWord = sc.nextLine();
+        String originalWord = sc.nextLine().toLowerCase();
 
         StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < testWord.length(); i++) {
-            char ch = testWord.charAt(i);
-            if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
-                sb.append(Character.toLowerCase(ch)); 
+        for (char ch : originalWord.toCharArray()) {
+            if (Character.isLetter(ch)) {
+                sb.append(ch);
             }
         }
-
         String processedWord = sb.toString();
-
-        char[] letters = new char[processedWord.length()];
-
-        for (int i = 0; i < processedWord.length(); i++) {
-            letters[i] = processedWord.charAt(i);
-        }
-
-        boolean palindromeChecker = true;
-
-        for (int i = 0; i < processedWord.length() / 2; i++) {
-            if (letters[i] != letters[processedWord.length() - i - 1]) {
-                palindromeChecker = false;
+        int processedlength = processedWord.length();
+        boolean palindrome = true;
+        for (int i = 0; i < processedlength / 2; i++) {
+            if (processedWord.charAt(i) != processedWord.charAt(processedlength - i - 1)) {
+                palindrome = false;
             }
         }
-
-        if (palindromeChecker) {
-            System.out.println("The word: " + processedWord + " is a palindrome.");
-        } else {
-            System.out.println("The word: " + processedWord + " is not a palindrome.");
-        }
+        System.out.println("The word: " + processedWord + (palindrome ? " is a palindrome." : " is not a palindrome."));
     }
 }
