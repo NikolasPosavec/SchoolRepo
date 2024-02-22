bb1c088f694d745b5fcb06f9df419d9af6fc1258
Nikolas Posavec <nikolas.posavec@gmail.com>
2024-02-22 13:09:00 -0500

    PalindromeNP

--- PalindromeNP.java
+++ PalindromeNP.java
@@ -1,34 +1,40 @@
 import java.util.Scanner;
 public class PalindromeNP {
     public static void main(String[] args) {
-        
         Scanner sc = new Scanner(System.in);
 
         System.out.println("What is your word?");
         String testWord = sc.nextLine();
-        char [] letters = new char[testWord.length()];
 
-        testWord = testWord.replaceAll("[,\\s!]", "");
-        testWord = testWord.toLowerCase();
+        StringBuilder sb = new StringBuilder();
 
-        for(int i=0; i<testWord.length(); i++){
-            letters[i] = testWord.charAt(i);
-        }
-
-        boolean palindromechecker = true;
-
-        for(int i = 0; i<testWord.length()/2; i++){
-            if (letters[i] != letters[testWord.length() - i - 1]) {
-                palindromechecker = false;
+        for (int i = 0; i < testWord.length(); i++) {
+            char ch = testWord.charAt(i);
+            if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+                sb.append(Character.toLowerCase(ch)); 
             }
         }
-        
-        if (palindromechecker) {
-            System.out.println("The word: " + testWord + " is a palindrome.");
+
+        String processedWord = sb.toString();
+
+        char[] letters = new char[processedWord.length()];
+
+        for (int i = 0; i < processedWord.length(); i++) {
+            letters[i] = processedWord.charAt(i);
+        }
+
+        boolean palindromeChecker = true;
+
+        for (int i = 0; i < processedWord.length() / 2; i++) {
+            if (letters[i] != letters[processedWord.length() - i - 1]) {
+                palindromeChecker = false;
+            }
+        }
+
+        if (palindromeChecker) {
+            System.out.println("The word: " + processedWord + " is a palindrome.");
         } else {
-            System.out.println("The word: " + testWord + " is not a palindrome.");
+            System.out.println("The word: " + processedWord + " is not a palindrome.");
         }
     }
 }
-    
-
--- TEST.java
+++ TEST.java
@@ -1,30 +1,41 @@
 import java.util.Scanner;
-public class TEST 
-{
-    public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
 
-    System.out.println("What is your word?");
-    String testWord = sc.nextLine();
-    char[] letters = new char[testWord.length()];
+public class TEST {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    for (int i = 0; i < testWord.length(); i++) {
-        char ch = testWord.charAt(i);
-        letters[i] = ch;
-    }
+        System.out.println("What is your word?");
+        String testWord = sc.nextLine();
 
-    boolean isPalindrome = true;
-    for (int i = 0; i < testWord.length() / 2; i++) {
-        if (letters[i] != letters[testWord.length() - i - 1]) {
-            isPalindrome = false;
-            break;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < testWord.length(); i++) {
+            char ch = testWord.charAt(i);
+            if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+                sb.append(Character.toLowerCase(ch)); 
+            }
+        }
+
+        String processedWord = sb.toString();
+
+        char[] letters = new char[processedWord.length()];
+
+        for (int i = 0; i < processedWord.length(); i++) {
+            letters[i] = processedWord.charAt(i);
+        }
+
+        boolean palindromeChecker = true;
+
+        for (int i = 0; i < processedWord.length() / 2; i++) {
+            if (letters[i] != letters[processedWord.length() - i - 1]) {
+                palindromeChecker = false;
+            }
+        }
+
+        if (palindromeChecker) {
+            System.out.println("The word: " + processedWord + " is a palindrome.");
+        } else {
+            System.out.println("The word: " + processedWord + " is not a palindrome.");
         }
     }
-
-    if (isPalindrome) {
-        System.out.println("The word: " + testWord + " is a palindrome.");
-    } else {
-        System.out.println("The word: " + testWord + " is not a palindrome.");
-    }
-}
 }
