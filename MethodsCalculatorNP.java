f7dd6eeb53f9966c9a2315e937ae34cf82ca6bec
Nikolas Posavec <nikolas.posavec@gmail.com>
2024-03-15 08:19:35 -0400

    MethodsCalculatorNP

--- MethodsCalculatorNP.java
+++ MethodsCalculatorNP.java
@@ -1,43 +1,70 @@
 import java.util.Scanner;
+
 public class MethodsCalculatorNP {
-    public static void main (String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Do you want to add(+), subtract(-), multiply(*), or divide(/)? Type the corresponding symbol.");
-        String symbol = sc.nextLine();
-        System.out.println("Write the first number you would like in the equation: ");
-        double d = sc.nextDouble();
-        System.out.println("Write the second number you would like in the equation: ");
-        double e = sc.nextDouble();
-        if(symbol.equals("+")){
-            double ans = add(d, e);
-            System.out.println(ans);
-        }else if(symbol.equals("-")){
-            double ans1 = sub(d, e);
-            System.out.println(ans1);
-        }else if(symbol.equals("*")){
-            double ans2 = mult(d, e);
-            System.out.println(ans2);
-        }else if(symbol.equals("/")){
-            double ans3 = div(d, e);
-            System.out.println(ans3);
-        }else{
-            System.out.println("Invalid symbol, try again");
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Which calculator would you like to use? 1: Quadratic equation 2: System of equations");
+        int calcop = sc.nextInt();
+
+        switch (calcop) {
+            case 1:
+                Quadratic();
+                break;
+            case 2:
+                solveSystemOfEquations();
+                break;
+            default:
+                System.out.println("Error: Not an option");
         }
     }
-    public static double add(double d, double e) {
-        double sum = d+e;
-        return(sum);
+
+    public static void Quadratic() {
+        System.out.println("Enter the coefficients (a, b, c) of the quadratic equation: ");
+        System.out.print("a: ");
+        double a = sc.nextDouble();
+        System.out.print("b: ");
+        double b = sc.nextDouble();
+        System.out.print("c: ");
+        double c = sc.nextDouble();
+        double discriminant = b * b - 4 * a * c;
+        if (discriminant >= 0) {
+            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+
+            System.out.println("The roots are real and different.");
+            System.out.println("Root 1 = " + root1);
+            System.out.println("Root 2 = " + root2);
+        } else {
+            System.out.println("There are no real solutions.");
+        }
     }
-    public static double sub(double d, double e) {
-        double diff = d-e;
-        return(diff);
-    }
-    public static double mult(double d, double e){
-        double product = d*e;
-        return(product);
-    }
-    public static double div(double d, double e){
-        double quo = d/e;
-        return(quo);
+
+    public static void solveSystemOfEquations() {
+        System.out.println("Enter coefficients for the first equation (ax + by = c):");
+        System.out.print("a: ");
+        double a1 = sc.nextDouble();
+        System.out.print("b: ");
+        double b1 = sc.nextDouble();
+        System.out.print("c: ");
+        double c1 = sc.nextDouble();
+
+        System.out.println("Enter coefficients for the second equation (dx + ey = f):");
+        System.out.print("d: ");
+        double a2 = sc.nextDouble();
+        System.out.print("e: ");
+        double b2 = sc.nextDouble();
+        System.out.print("f: ");
+        double c2 = sc.nextDouble();
+
+        double determinant = a1 * b2 - a2 * b1;
+
+        if (determinant == 0) {
+            System.out.println("The system of equations has no unique solution.");
+        } else {
+            double x = (c1 * b2 - c2 * b1) / determinant;
+            double y = (a1 * c2 - a2 * c1) / determinant;
+            System.out.println("Solution: x = " + x + ", y = " + y);
+        }
     }
 }
--- TEST.java
+++ TEST.java
@@ -1,17 +1,10 @@
 import java.util.Scanner;
 
+
 public class TEST {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("What's your word?");
-        String strF = sc.nextLine();
-        char[] strFa = strF.toCharArray();
-        char[] strRa = new char[strF.length()];
-        for (int i = 0; i < strF.length(); i++) {
-            strRa[strF.length() - 1 - i] = strFa[i];
-        }
-        String backward = new String(strRa);
-        System.out.println(backward);
+        factorQuadratic();
     }
+
+    
 }
