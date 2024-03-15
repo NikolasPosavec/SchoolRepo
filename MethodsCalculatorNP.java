b950e39a2cef530ed3c6fba651e75b0fdd2e4a25
Nikolas Posavec <nikolas.posavec@gmail.com>
2024-03-15 09:09:53 -0400

    MethodsCalculatorNP final copy

--- MethodsCalculatorNP.java
+++ MethodsCalculatorNP.java
@@ -4,7 +4,7 @@ public class MethodsCalculatorNP {
     private static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Which calculator would you like to use? 1: Quadratic equation 2: System of equations");
+        System.out.println("Which calculator would you like to use? 1: Quadratic equation 2: System of equations 3: Perfect square trinomial");
         int calcop = sc.nextInt();
 
         switch (calcop) {
@@ -14,32 +14,40 @@ public class MethodsCalculatorNP {
             case 2:
                 solveSystemOfEquations();
                 break;
+            case 3:
+                perfectSquareTrinomial();
+                break;
             default:
                 System.out.println("Error: Not an option");
         }
     }
 
     public static void Quadratic() {
-        System.out.println("Enter the coefficients (a, b, c) of the quadratic equation: ");
-        System.out.print("a: ");
+        System.out.println("Enter coefficients a, b, and c:");
         double a = sc.nextDouble();
-        System.out.print("b: ");
         double b = sc.nextDouble();
-        System.out.print("c: ");
         double c = sc.nextDouble();
+
         double discriminant = b * b - 4 * a * c;
-        if (discriminant >= 0) {
+
+        if (discriminant > 0) {
             double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
             double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-
-            System.out.println("The roots are real and different.");
-            System.out.println("Root 1 = " + root1);
-            System.out.println("Root 2 = " + root2);
+            root1 = roundToTwoDecimals(root1);
+            root2 = roundToTwoDecimals(root2);
+            System.out.println("Real roots: " + root1 + " and " + root2);
+        } else if (discriminant == 0) {
+            double root = -b / (2 * a);
+            root = roundToTwoDecimals(root);
+            System.out.println("Real and same root: " + root);
         } else {
-            System.out.println("There are no real solutions.");
+            double realPart = -b / (2 * a);
+            double imaginaryPart = Math.sqrt(Math.abs(discriminant)) / (2 * a);
+            realPart = roundToTwoDecimals(realPart);
+            imaginaryPart = roundToTwoDecimals(imaginaryPart);
+            System.out.println("Imaginary roots: " + realPart + " + " + imaginaryPart + "i and " + realPart + " - " + imaginaryPart + "i");
         }
     }
-
     public static void solveSystemOfEquations() {
         System.out.println("Enter coefficients for the first equation (ax + by = c):");
         System.out.print("a: ");
@@ -64,7 +72,28 @@ public class MethodsCalculatorNP {
         } else {
             double x = (c1 * b2 - c2 * b1) / determinant;
             double y = (a1 * c2 - a2 * c1) / determinant;
+            x = roundToTwoDecimals(x);
+            y = roundToTwoDecimals(y);
             System.out.println("Solution: x = " + x + ", y = " + y);
         }
     }
+    public static void perfectSquareTrinomial() {
+        System.out.println("Enter your coefficients for the equation (ax+b)^2:");
+        System.out.print("a: ");
+        double a = sc.nextDouble();
+        System.out.print("b: ");
+        double b = sc.nextDouble();
+
+        double axSquared = a * a;
+        double twoabx = 2 * a * b;
+        double bb = b * b;
+        axSquared = roundToTwoDecimals(axSquared);
+        twoabx = roundToTwoDecimals(twoabx);
+        bb = roundToTwoDecimals(bb);
+        System.out.println("Solution: " + axSquared + "x^2 + " + twoabx + "x + " + bb);
+    }
+    public static double roundToTwoDecimals(double number) {
+        return Math.round(number * 100.0) / 100.0;
+    }
 }
+    
