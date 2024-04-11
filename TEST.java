7bd5466756a6dc55f353b30ccda1fd739d1c013a
Nikolas Posavec <nikolas.posavec@gmail.com>
2024-04-11 11:43:41 -0400

    PizzaTester

--- TEST.java
+++ TEST.java
@@ -2,8 +2,21 @@ import java.util.Scanner;
 
 
 public class TEST {
-    public static void main(String[] args) {
-        factorQuadratic();
+    public static void main(String[] args){
+        boolean answer = findPair(4,12,4);
+    }
+    public static boolean findPair(int first, int second, int third) {
+        boolean answer = true;
+        if(first==second || first==third || second==third){
+            answer = true;
+            if(first==second || first==third){
+                System.out.println(first);
+            }else{
+                System.out.println(second);
+            return;
+
+        }
+        
     }
 
     
