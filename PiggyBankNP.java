13f9c976a887bda2533b844ac3d26d83eeaf8070
Nikolas Posavec <nikolas.posavec@gmail.com>
2024-05-01 14:31:20 -0400

    update to PiggyBankNP

--- PiggyBankNP.java
+++ PiggyBankNP.java
@@ -8,7 +8,8 @@ class PiggyBank {
     private int coinTotalValue;
     private int amountOfEachCoin;
     
-    PiggyBank(){
+    PiggyBank(int totval){
+        coinTotalValue = totval;
         pennies = 0;
         nickels = 0;
         dimes = 0;
@@ -46,35 +47,56 @@ class PiggyBank {
         coinTotalValue = coinTotalValue/100;
         return coinTotalValue;
     }
+
+    void sortCoins(){
+        quarters = coinTotalValue/25;
+        coinTotalValue = coinTotalValue - quarters*25;
+        dimes = coinTotalValue/10;
+        coinTotalValue = coinTotalValue - dimes*10;
+        nickels = coinTotalValue/5;
+        coinTotalValue = coinTotalValue - nickels*5;
+        pennies = coinTotalValue;
+    }
+
+    public String toString(){
+        System.out.println("You have " + pennies + " pennies, " + nickels + " nickels, "+ dimes + " dimes, and" + quarters + " quarters.");
+        System.out.println("Your piggybank has a value of " + coinTotalValue + "dollars");
+    }
 }
 
     public class PiggyBankNP{
         public static void main(String[] args){
             Scanner sc = new Scanner(System.in);
             
-            System.out.println("Would you like to start with zero coins(type 0) or do you want to set your amount(type 1)?");
+            System.out.println("Would you like to start with a cent value (type 0) or do you want to set your amount(type 1)?");
             int bankchoice = sc.nextInt();
 
             switch (bankchoice) {
                 case 0:
-                    PiggyBank bank1 = new PiggyBank();
-                    System.out.println("How many pennies would you like to add to the bank?");
-                    int num1 = sc.nextInt();
-                    System.out.println("How many nickels would you like to add to the bank?");
-                    int num2 = sc.nextInt();
-                    System.out.println("How many dimes would you like to add to the bank?");
-                    int num3 = sc.nextInt();
-                    System.out.println("How many quarters would you like to add to the bank?");
-                    int num4 = sc.nextInt();
-                    
-                    bank1.addCoins(num1, num2, num3, num4);
-                    double totalvalue = bank1.calcCoins();
+                    System.out.println("What is the total value of coins in cents?");
+                    int coinVal = sc.nextInt();
+                    PiggyBank bank1 = new PiggyBank(coinVal);
+                    bank1.sortCoins();
 
-                    System.out.println("You have " + num1 + " pennies, " + num2 + " nickels, "+ num3 + " dimes, and" + num4 + " quarters.");
-                    System.out.println("Your piggybank has a value of " + totalvalue + "dollars");
-                    break;
+                    System.out.println("You have " + quarters + " quarters, " + dimes " dimes, " + nickels + " nickels, " + "and " + pennies + " pennies.");
+
+                    }
             
                 case 1:
+                    System.out.println("How many pennies would you like to add to the bank?");
+                    int pens = sc.nextInt();
+                    System.out.println("How many nickels would you like to add to the bank?");
+                    int nicks = sc.nextInt();
+                    System.out.println("How many dimes would you like to add to the bank?");
+                    int dimes1 = sc.nextInt();
+                    System.out.println("How many quarters would you like to add to the bank?");
+                    int quarts = sc.nextInt();
+                    PiggyBank bank2 = new PiggyBank(pens, nicks, dimes1, quarts);
+                    
+                    double totalvalue1 = bank2.calcCoins();
+
+                    System.out.println("You have " + pens + " pennies, " + nicks + " nickels, "+ dimes1 + " dimes, and" + quarts + " quarters.");
+                    System.out.println("Your piggybank has a value of " + totalvalue1 + "dollars");
                     break;
             }
 
