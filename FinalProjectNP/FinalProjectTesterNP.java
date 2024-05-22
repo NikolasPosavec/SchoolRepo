8282084acb78cd41b11dde938a369a13bbe4b3bf
Nikolas Posavec <nikolas.posavec@gmail.com>
2024-05-22 13:07:15 -0400

    commit

--- FinalProjectNP/FinalProjectTesterNP.java
+++ FinalProjectNP/FinalProjectTesterNP.java
@@ -4,7 +4,7 @@ public class FinalProjectTesterNP {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        ArrayList<FootWearSuper> footwearList = new ArrayList<>();
+        ArrayList<FootWearSuper> footwearList = new ArrayList<FootWearSuper>();
 
         footwearList.add(new Shoes("Sperry", 9.5, "Black", 50.00, "leather", true));
         footwearList.add(new Shoes("Nike", 10.0, "Brown", 60.00,  "canvas", true));
@@ -23,32 +23,37 @@ public class FinalProjectTesterNP {
         System.out.println("Brand: " + footwear.getBrand());
         System.out.println("Size: " + footwear.getSize());
         System.out.println("Color: " + footwear.getColor());
-        System.out.println("Price: $" + footwear.getPrice());
         if (footwear instanceof Shoes) {
             Shoes shoes = (Shoes) footwear;
             System.out.println("Material: " + shoes.getMaterial());
             System.out.println("Has Laces: " + shoes.hasLaces());
+            System.out.println("Price: $" + shoes.getPrice());
         } else if (footwear instanceof Sneakers) {
             Sneakers sneakers = (Sneakers) footwear;
             System.out.println("Material: " + sneakers.getMaterial());
             System.out.println("Has Laces: " + sneakers.hasLaces());
             System.out.println("Has Arch Support: " + sneakers.hasArchSupport());
+            System.out.println("Price: $" + sneakers.getPrice());
         } else if (footwear instanceof Boots) {
             Boots boots = (Boots) footwear;
             System.out.println("Material: " + boots.getMaterial());
             System.out.println("Has Laces: " + boots.hasLaces());
             System.out.println("Height: " + boots.getHeight() + " inches");
+            System.out.println("Price: $" + boots.getPrice());
         } else if (footwear instanceof Sandals) {
             Sandals sandals = (Sandals) footwear;
             System.out.println("Has Straps: " + sandals.hasStraps());
+            System.out.println("Price: $" + sandals.getPrice());
         } else if (footwear instanceof Slides) {
             Slides slides = (Slides) footwear;
             System.out.println("Has Straps: " + slides.hasStraps());
             System.out.println("Has Back Strap: " + slides.hasBackStrap());
+            System.out.println("Price: $" + slides.getPrice());
         } else if (footwear instanceof FlipFlops) {
             FlipFlops flipFlops = (FlipFlops) footwear;
             System.out.println("Is Waterproof: " + flipFlops.isWaterproof());
             System.out.println("Has Straps: " + flipFlops.hasStraps());
+            System.out.println("Price: $" + flipFlops.getPrice());
         }
         System.out.println();
     }
