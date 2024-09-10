f23c48fb3f5bce293486597fa810b6c38b17f812
Nikolas Posavec <nikolas.posavec@gmail.com>
2024-09-10 18:45:44 -0400

    test

--- APCS/FirstThreePrograms.java
+++ APCS/FirstThreePrograms.java
@@ -1,4 +1,5 @@
 package APCS;
+
 import java.util.Scanner;
 
 public class FirstThreePrograms 
@@ -23,9 +24,41 @@ public class FirstThreePrograms
     }   
 
 
-public boolean rightTriangle(int firstSide, int secondSide, int thirdSide)
+public static boolean rightTriangle(int firstSide, int secondSide, int thirdSide)
 {
-    
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    if(firstSide > secondSide && firstSide > thirdSide)
+    {
+        c = firstSide;
+    } else {
+        a = firstSide;
+    }
+    if(secondSide > firstSide && secondSide > thirdSide)
+    {
+        c = secondSide;
+    } else {
+        b = secondSide;
+    }
+    if(a == 0)
+    {
+        a = thirdSide;
+    } else if(b == 0){
+        b = thirdSide;
+    } else if(c == 0){
+        c = thirdSide;
+    }
+    double result = (a*a) + (b*b);
+    double check = (c*c);
+    if(result == check)
+    {
+        return true;
+        System.out.println("It is a right triangle!");
+    } else {
+        return false;
+        System.out.println("It is not a right triangle!")
+    }
 }
 
 }
