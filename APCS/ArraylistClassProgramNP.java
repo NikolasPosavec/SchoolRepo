a9942705d75552914559d32038aaa91568586e0f
Nikolas Posavec <nikolas.posavec@gmail.com>
2024-09-24 14:38:12 -0400

    done arraylistthing

--- APCS/ArraylistClassProgramNP.java
+++ APCS/ArraylistClassProgramNP.java
@@ -3,52 +3,41 @@ import java.util.*;
 
 public class ArraylistClassProgramNP 
 {
+    static Scanner sc = new Scanner(System.in);
+    static ArrayList<String> Words = new ArrayList<String>();
+    static ArrayList<String> NewList = new ArrayList<String>();
+    static int num = 3;
+
     public static void main(String[] args) 
     {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Do you want to use raw array lists (type 1) or generic array lists (type 2)?");
-        int control = sc.nextInt();
-
-        switch(control)
-        {
-            case 1:
-            String temp;
-            int numOfStrings = 0;
-            do{
-                System.out.println("Type in a word (type negativeone to stop): ");
-                temp = sc.nextLine();
-
-            }while(temp != "negativeone");
-
-            for(int i = 0; i <= numOfStrings; i++)
-            {
-                if(temp != "negativeone")
-                {
-
-                }
-            }
-            break;
-            case 2:
-
-            break;
-        }
+        NewWay();
     }
 
-    public static void OldWay()
+    public static void NewWay()
     {
-        ArrayList list = new ArrayList();
-        String word;
-        int count;
+        String temp;
 
-        for(int i = 0; i < list.size(); i++)
-        {
-            word = (String)list.get(i);
-            
-            if(word.length() == num)
-            {
-                count++;
+        do {
+            System.out.println("Type in a word (type negativeone to stop): ");
+            temp = sc.nextLine();
+
+            if (!temp.equals("negativeone")) {
+                Words.add(temp);
             }
+
+        } while (!temp.equals("negativeone"));
+
+        for (String word : Words)
+        {
+            if (word.length() == num)
+            {
+                NewList.add(word);
+            }
+        }
+
+        while (!NewList.isEmpty())
+        {
+            System.out.println(NewList.remove(0));
         }
     }
 }
