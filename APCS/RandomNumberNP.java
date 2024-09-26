51a4614bc045dc1f5e8c68c3507e37b1e305c216
Nikolas Posavec <nikolas.posavec@gmail.com>
2024-09-26 13:48:46 -0400

    for each loop edit

--- APCS/RandomNumberNP.java
+++ APCS/RandomNumberNP.java
@@ -20,13 +20,18 @@ public class RandomNumberNP
 
         System.out.println("List of randomly generated numbers: ");
 
-        for(int i = 0; i < list.size(); i++)
+        for(int x: list)
+        {
+            System.out.println(x);
+            total += x;
+        }
+        /*for(int i = 0; i < list.size(); i++) <- Old for loop
         {
             //int y = (Integer)list.get(i); <-Old way
             int y = list.get(i);
             System.out.println(list.get(i));
             total += y;
-        }
+        }*/
         
         avg = (double)total/list.size();
         System.out.println("The average of " + limit + " randomly generated numbers is: "+ avg);
