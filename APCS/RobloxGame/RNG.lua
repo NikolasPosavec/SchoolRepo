9e9212d8df5ee14086bd43acd8a8cdab07270164
Nikolas Posavec <nikolas.posavec@gmail.com>
2024-10-02 11:45:06 -0400

    playing w lua

--- APCS/RobloxGame/RNG.lua
+++ APCS/RobloxGame/RNG.lua
@@ -1,1 +1,12 @@
-print("Hello World")
+j = "common"
+
+function RNG(n)
+    n = math.random(1000000)
+    if n < 100000 then
+        return j
+    end 
+    if n >= 100000 and n < 300000 then
+        j = "uncommon"
+        return j
+    end
+end
