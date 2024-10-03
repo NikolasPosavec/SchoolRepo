3e78d0f7bc5088cdaf4c74e4c074ac64b057f63e
Nikolas Posavec <nikolas.posavec@gmail.com>
2024-10-02 20:46:46 -0400

    fixed fruit button being doubled

--- APCS/RobloxGame/FruitDisplayButton.lua
+++ APCS/RobloxGame/FruitDisplayButton.lua
@@ -9,14 +9,3 @@ end
 
 -- Connect the BindableEvent to the function
 rarityEvent.Event:Connect(onRarityReceived)
-local button = script.Parent
-local rarityEvent = game:GetService("ReplicatedStorage"):WaitForChild("RarityPicked")  -- Reference the BindableEvent
-
--- Function to update the FRUIT button text when the event is triggered
-local function onRarityReceived(rarity)
-	button.Text = rarity  -- Set the button text to the received rarity
-	print("Rarity received: " .. rarity)
-end
-
--- Connect the BindableEvent to the function
-rarityEvent.Event:Connect(onRarityReceived)
