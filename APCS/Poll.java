d9d0495a3c7959eaf13594656adfd0801022acac
Nikolas Posavec <nikolas.posavec@gmail.com>
2024-10-03 14:29:42 -0400

    poll prgm done

--- APCS/Poll.java
+++ APCS/Poll.java
@@ -11,105 +11,117 @@ public class Poll {
       pollFrame.setSize(900,900);
       pollFrame.setLayout(null);
       pollFrame.setVisible(true); 
+      pollFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
    }
 }
-   
+
 class PollWindow extends JPanel implements ActionListener {
-   JButton button1, button2, button3;
-   int count1, count2, count3;
-  
-  public PollWindow()
-  {
-    button1 = new JButton(" Tami");
-    button1.setToolTipText("Vote for Tami");
-    button1.addActionListener(this);
+   JButton button1, button2, button3, button4;
+   int count1, count2, count3, count4;
 
-    button2 = new JButton("Brian");
-    button2.setToolTipText("Vote for Brian");
-    button2.addActionListener(this);
+   public PollWindow() {
+      button1 = new JButton("Nick");
+      button1.setToolTipText("Vote for Nick");
+      button1.addActionListener(this);
 
-    button3 = new JButton(" Liz ");
-    button3.setToolTipText("Vote for Liz");
-    button3.addActionListener(this);
+      button2 = new JButton("Dan");
+      button2.setToolTipText("Vote for Dan");
+      button2.addActionListener(this);
 
-    add(button1);
-    add(button2);
-    add(button3);
-  }
+      button3 = new JButton("Eric");
+      button3.setToolTipText("Vote for Eric");
+      button3.addActionListener(this);
 
-  /**
-   *  Processes button events
-   */
-  public void actionPerformed(ActionEvent e)
-  {
-    JButton button = (JButton)e.getSource();
+      button4 = new JButton("Onyeka");
+      button4.setToolTipText("Vote for Onyeka");
+      button4.addActionListener(this);
 
-    if (button == button1)
-      count1++;
-    else if (button == button2)
-      count2++;
-    else if (button == button3)
-      count3++;
+      add(button1);
+      add(button2);
+      add(button3);
+      add(button4);
+   }
 
-    repaint();
-  }
-  
-  /**
-   *  Displays the numbers and the pie chart
-   */
-  @Override
-  public void paintComponent(Graphics g)
-  {
-    super.paintComponent(g);
+   public void actionPerformed(ActionEvent e) {
+      JButton button = (JButton) e.getSource();
 
-    final int xStep = getWidth()/8;
+      if (button == button1)
+         count1++;
+      else if (button == button2)
+         count2++;
+      else if (button == button3)
+         count3++;
+      else if (button == button4)
+         count4++;
 
-    // Display numbers
-    int y = 575;
-    g.drawString(String.valueOf(count1), 2*xStep, y);
-    g.drawString(String.valueOf(count2), 4*xStep, y);
-    g.drawString(String.valueOf(count3), 6*xStep, y);
+      repaint();
+   }
 
-    // Display legend (colors)
-    y = 580;
-    g.setColor(Color.red);
-    g.fillRect(2*xStep, y, 10, 10);
-    g.setColor(Color.blue);
-    g.fillRect(4*xStep, y, 10, 10);
-    g.setColor(Color.green);
-    g.fillRect(6*xStep, y, 10, 10);
+   @Override
+   public void paintComponent(Graphics g) {
+      super.paintComponent(g);
 
-    // Display pie chart
-    int total = count1 + count2 + count3;
-    if (total == 0)
-      return;
+      final int xStep = getWidth() / 10;
+      int y = 575;
+      g.drawString(String.valueOf(count1), 2 * xStep, y);
+      g.drawString(String.valueOf(count2), 4 * xStep, y);
+      g.drawString(String.valueOf(count3), 6 * xStep, y);
+      g.drawString(String.valueOf(count4), 8 * xStep, y);
 
-    final int size = 4*xStep, x = 4*xStep - size/2;
-    y = 70;
-    g.setColor(Color.lightGray);
-    g.fillOval(x, y, size, size);
+      y = 580;
+      g.setColor(Color.pink);
+      g.fillRect(2 * xStep, y, 10, 10);
+      g.setColor(Color.orange);
+      g.fillRect(4 * xStep, y, 10, 10);
+      g.setColor(Color.black);
+      g.fillRect(6 * xStep, y, 10, 10);
+      g.setColor(Color.blue);
+      g.fillRect(8 * xStep, y, 10, 10);
+      g.setColor(Color.black);
+      g.drawString("Nick", 2 * xStep + 15, y + 10);
+      g.drawString("Dan", 4 * xStep + 15, y + 10);
+      g.drawString("Eric", 6 * xStep + 15, y + 10);
+      g.drawString("Onyeka", 8 * xStep + 15, y + 10);
 
-    int degrees, from = -90;
-    degrees = countToDegrees(count1, total);
-    g.setColor(Color.red);
-    g.fillArc(x, y, size, size, from, degrees);
-    from += degrees;
-    degrees = countToDegrees(count2, total);
-    g.setColor(Color.blue);
-    g.fillArc(x, y, size, size, from, degrees);
-    from += degrees;
-    degrees = countToDegrees(count3, total);
-    g.setColor(Color.green);
-    g.fillArc(x, y, size, size, from, degrees);
-  }
+      int total = count1 + count2 + count3 + count4;
+      if (total == 0)
+         return;
 
-  /**
-   *  Converts the count as a fraction of the total into
-   *  the size of the pie slice in degrees.
-   */
-  private int countToDegrees(int count, int total)
-  {
-    return (int)((double)count / (double)total * 360.);
-  }
+      final int size = 4 * xStep, x = 5 * xStep - size / 2;
+      y = 70;
+      g.setColor(Color.lightGray);
+      g.fillOval(x, y, size, size);
+
+      int degrees, from = 0;
+      degrees = countToDegrees(count1, total);
+      g.setColor(Color.pink);
+      g.fillArc(x, y, size, size, from, degrees);
+      from += degrees;
+      degrees = countToDegrees(count2, total);
+      g.setColor(Color.orange);
+      g.fillArc(x, y, size, size, from, degrees);
+      from += degrees;
+      degrees = countToDegrees(count3, total);
+      g.setColor(Color.black);
+      g.fillArc(x, y, size, size, from, degrees);
+      from += degrees;
+      degrees = countToDegrees(count4, total);
+      g.setColor(Color.blue);
+      g.fillArc(x, y, size, size, from, degrees);
+
+      
+    g.setColor(Color.black);
+      y += 450; 
+        if (total > 0) {
+          g.drawString("Nick: " + String.format("%.1f", (count1 / (double) total) * 100) + "%", 2 * xStep, y);
+          g.drawString("Dan: " + String.format("%.1f", (count2 / (double) total) * 100) + "%", 4 * xStep, y);
+          g.drawString("Eric: " + String.format("%.1f", (count3 / (double) total) * 100) + "%", 6 * xStep, y);
+          g.drawString("Onyeka: " + String.format("%.1f", (count4 / (double) total) * 100) + "%", 8 * xStep, y);
+        }
+
+   }
+
+   private int countToDegrees(int count, int total) {
+      return (int) ((double) count / (double) total * 360.);
+   }
 }
-
