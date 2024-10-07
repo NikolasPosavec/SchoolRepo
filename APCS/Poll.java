c7232b033dfd497f24c7bd30f92b244996f4c73b
Nikolas Posavec <nikolas.posavec@gmail.com>
2024-10-07 09:37:23 -0400

    clockwise in poll prgm

--- APCS/Poll.java
+++ APCS/Poll.java
@@ -60,14 +60,14 @@ class PollWindow extends JPanel implements ActionListener {
    @Override
    public void paintComponent(Graphics g) {
       super.paintComponent(g);
-
+   
       final int xStep = getWidth() / 10;
       int y = 575;
       g.drawString(String.valueOf(count1), 2 * xStep, y);
       g.drawString(String.valueOf(count2), 4 * xStep, y);
       g.drawString(String.valueOf(count3), 6 * xStep, y);
       g.drawString(String.valueOf(count4), 8 * xStep, y);
-
+   
       y = 580;
       g.setColor(Color.pink);
       g.fillRect(2 * xStep, y, 10, 10);
@@ -82,41 +82,46 @@ class PollWindow extends JPanel implements ActionListener {
       g.drawString("Dan", 4 * xStep + 15, y + 10);
       g.drawString("Eric", 6 * xStep + 15, y + 10);
       g.drawString("Onyeka", 8 * xStep + 15, y + 10);
-
+   
       int total = count1 + count2 + count3 + count4;
       if (total == 0)
          return;
-
+   
       final int size = 4 * xStep, x = 5 * xStep - size / 2;
       y = 70;
-
-      int degrees, from = 0;
-      degrees = (int) countToDegrees(count1, total);
+   
+      //start from pos 90 and change angles to negative cus then it goes right
+      int degrees, from = 90; 
+      
+      degrees = -(int) countToDegrees(count1, total);
       g.setColor(Color.pink);
       g.fillArc(x, y, size, size, from, degrees);
       from += degrees;
-      degrees = (int) countToDegrees(count2, total);
+      
+      degrees = -(int) countToDegrees(count2, total);
       g.setColor(Color.orange);
       g.fillArc(x, y, size, size, from, degrees);
       from += degrees;
-      degrees = (int) countToDegrees(count3, total);
+      
+      degrees = -(int) countToDegrees(count3, total);
       g.setColor(Color.black);
       g.fillArc(x, y, size, size, from, degrees);
       from += degrees;
-      degrees = (int) countToDegrees(count4, total);
+      
+      degrees = -(int) countToDegrees(count4, total);
       g.setColor(Color.blue);
       g.fillArc(x, y, size, size, from, degrees);
-
+   
       g.setColor(Color.black);
       y += 450;
       if (total > 0) {
-          g.drawString("Nick: " + String.format("%.1f", (count1 / (double) total) * 100) + "%", 2 * xStep, y);
-          g.drawString("Dan: " + String.format("%.1f", (count2 / (double) total) * 100) + "%", 4 * xStep, y);
-          g.drawString("Eric: " + String.format("%.1f", (count3 / (double) total) * 100) + "%", 6 * xStep, y);
-          g.drawString("Onyeka: " + String.format("%.1f", (count4 / (double) total) * 100) + "%", 8 * xStep, y);
+         g.drawString("Nick: " + String.format("%.1f", (count1 / (double) total) * 100) + "%", 2 * xStep, y);
+         g.drawString("Dan: " + String.format("%.1f", (count2 / (double) total) * 100) + "%", 4 * xStep, y);
+         g.drawString("Eric: " + String.format("%.1f", (count3 / (double) total) * 100) + "%", 6 * xStep, y);
+         g.drawString("Onyeka: " + String.format("%.1f", (count4 / (double) total) * 100) + "%", 8 * xStep, y);
       }
    }
-
+   
    private double countToDegrees(int count, int total) {
       return ((double) count / total * 360.);
    }
