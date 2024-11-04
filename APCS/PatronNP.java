9f6d394ba87e5cd06feec252c1dca8a648af14e3
Nikolas Posavec <nikolas.posavec@gmail.com>
2024-11-03 22:34:27 -0500

    done Library

--- APCS/PatronNP.java
+++ APCS/PatronNP.java
@@ -1,48 +1,62 @@
 import java.util.ArrayList;
 
-public class PatronNP 
-{
+public class PatronNP {
     public String name;
-    ArrayList <String> books = new ArrayList<String>();
+    ArrayList<BookNP> books = new ArrayList<BookNP>();
 
-    public PatronNP()
-    {
-        this("", new ArrayList<String>());
+    public PatronNP() {
+        this("", new ArrayList<BookNP>());
     }
-    public PatronNP(String n, ArrayList<String> b)
-    {
+
+    public PatronNP(String n, ArrayList<BookNP> b) {
         name = n;
         books = b;
     }
-    public PatronNP(PatronNP p)
-    {
-        this(p.name, p.books);
+
+    public PatronNP(PatronNP p) {
+        this(p.name, new ArrayList<BookNP>(p.books));
     }
 
-    public void setName(String n)
-    {
+    public void setName(String n) {
         name = n;
     }
-    public String getName()
-    {
+
+    public String getName() {
         return name;
     }
-    public void setBooks(ArrayList<String> b)
-    {
+
+    public void setBooks(ArrayList<BookNP> b) {
         books = b;
     }
-    public ArrayList<String> getBooks()
-    {
+
+    public ArrayList<BookNP> getBooks() {
         return books;
     }
 
+    public void addBook(BookNP book) {
+        books.add(book);
+    }
+
+    public void returnBook(BookNP book) {
+        books.remove(book);
+    }
+
     @Override
-    public String toString()
-    {
+    public String toString() {
         return String.format(
             "Name: %s%n" +
             "Books: %s%n",
             name, books
         );
+    }
+
+    public void borrowBook(BookNP book) {
+        if (!books.contains(book)) {
+            books.add(book);
+        }
+    }
+
+    public BookNP[] getBorrowedBooks() {
+        return books.toArray(new BookNP[0]);
     }
 }
