1c0141c59ece3c012cc50798aa7d20b5d9b78e0c
Nikolas Posavec <155776636+NikolasPosavec@users.noreply.github.com>
2024-11-19 10:49:55 -0500

    Update Term.java

--- APCS/Portfolio/CalculusCalc/Term.java
+++ APCS/Portfolio/CalculusCalc/Term.java
@@ -5,16 +5,24 @@ public class Term
 {
     public String TopOfFunct;
     public String BotOfFunct;
+    public String OuterOp;
+    public String[] TermBuilt;
 
     public Term()
     {
         TopOfFunct = "";
         BotOfFunct = "";
+        OuterOp = "";
+        TermBuilt = [TopOfFunct, BotOfFunct, OuterOp];
     }
-    public Term(String T, String B)
+    public Term(String T, String B, String O)
     {
         TopOfFunct = T;
         BotOfFunct = B;
+        OuterOp = O;
+        TermBuilt = [TopOfFunct, BotOfFunct, OuterOp];
     }
+    
+    public String TermBuilder
    
 }
