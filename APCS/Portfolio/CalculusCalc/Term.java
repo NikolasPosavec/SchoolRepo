b9168d3ea72d1a3fe87cd2938c440f616d7675b3
Nikolas Posavec <nikolas.posavec@gmail.com>
2024-11-19 10:15:04 -0500

    derivate calc term class made

---
+++ APCS/Portfolio/CalculusCalc/Term.java
@@ -0,0 +1,20 @@
+package Portfolio.CalculusCalc;
+import java.util.*;
+
+public class Term
+{
+    public String TopOfFunct;
+    public String BotOfFunct;
+
+    public Term()
+    {
+        TopOfFunct = "";
+        BotOfFunct = "";
+    }
+    public Term(String T, String B)
+    {
+        TopOfFunct = T;
+        BotOfFunct = B;
+    }
+   
+}
