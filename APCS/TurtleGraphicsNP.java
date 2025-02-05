4f4d7437bcc43c942db7e93103091895a2923e32
NikolasPosavec <nikolas.posavec@gmail.com>
2025-02-05 13:33:25 -0500

    jars in

---
+++ APCS/TurtleGraphicsNP.java
@@ -0,0 +1,31 @@
+package APCS;
+
+import TurtleGraphics.RainbowPen;
+import java.awt.Color;
+
+public class TurtleGraphicsNP 
+{
+    public static void main(String[] args) 
+    {
+        RainbowPen pen = new RainbowPen();
+        pen.setColor(Color.blue);
+
+        double sideStep = 175; 
+
+        for (int i = 0; i < 6; i++) 
+        {
+            for (int j = 0; j < 12; j++) 
+            { 
+                for (int k = 0; k < 6; k++) {
+                    pen.move(sideStep);
+                    pen.turn(60);
+                    pen.turn(5); 
+                }
+    
+                pen.turn(60);
+            }
+            
+            pen.turn(60);
+        }
+    }
+}
